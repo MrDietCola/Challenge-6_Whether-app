ffbbacebcb817d7068fa95e1a0dc5a35c71fbe56
AJ Seidler <ajseidler0526@gmail.com>
2023-10-02 23:45:35 -0500

    worked on searching twice functionality and the readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Challenge-6_Whether-app
+# Challenge-6_Weather-app
+
+
+
+## Description
+This is a weather app that allows search a city and then the app displays the corresponding weather and 5 day forecast for that city.
+
+
+
+## Installation
+
+N/A
+
+## Usage
+
+The purpose of this app is to provide the user with the ability to find weather in different areas as well as allow us to show our capabilities with using different api's. 
+
+## License
+
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,58 @@
 
+var searchInputBtn = $('#search-btn')
+var weatherApiUrl;
+var forecastApiUrl;
+var storedSearches = JSON.parse(localStorage.getItem("searches")) || [];
+
 searchInputBtn.on('click', function() {
+  clear()
   var searchInput = $('#search-input').val()
   console.log(searchInput);
   var geocodeApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchInput + ',ISO3166&limit=5&appid=8a3a661ef5387155c9e648d48ea692f7';
   var lat;
   var lon;
-    
+  storedSearches.push(searchInput);
+  localStorage.setItem("searches", (JSON.stringify(storedSearches)));
+  console.log(storedSearches);
   fetch(geocodeApiUrl)
+    .then(function (response) {
+    if (response.ok) {
+      return response.json();
+      } 
+    })
+    .then(function (locRes) {
+      console.log(locRes);
+      if (locRes.length === 1) {
+
+        const cardDiv = $('<div>')
+        const cardBodyDiv = $('<div>')
+        const dateH2 = $('<h2>')
+        cardDiv.addClass('card w-100 g-0 m-0 gx-0')
+        cardBodyDiv.addClass('card-body g-0')
+        dateH2.addClass('card-title')
+        dateH2.text('No city found')
+        $('#weather').append(cardDiv)
+        cardDiv.append(cardBodyDiv)
+        cardBodyDiv.append(dateH2)
+      } else {
+      for (var i = 0; i < locRes.length; i++) {
+        // printResults(locRes.results[i]);
+        if (locRes[i].country === 'US') {
+          lat = locRes[i].lat
+          lon = locRes[i].lon
+          console.log(lat + '   ' + lon);
+          forecastApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=8a3a661ef5387155c9e648d48ea692f7&units=imperial&limit=6';
+          weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=8a3a661ef5387155c9e648d48ea692f7&units=imperial&limit=6';
+          getForecast()
+          getWeather()
+        }
+      }
+    }
+    })
+})
+
+function getWeather() {
+  fetch(weatherApiUrl)
     .then(function (response) {
     if (response.ok) {
       return response.json();
@@ -14,23 +60,38 @@ searchInputBtn.on('click', function() {
     })
     .then(function (locRes) {
       console.log(locRes);
-      for (var i = 0; i < locRes.length; i++) {
-        // printResults(locRes.results[i]);
-        if (locRes[i].country === 'US') {
-          lat = locRes[i].lat
-          lon = locRes[i].lon
-          console.log(lat + '   ' + lon);
-          weatherApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=8a3a661ef5387155c9e648d48ea692f7&units=imperial&limit=6';
-          // console.log(weatherApiUrl);
-          getWeather()
-        }
-      }
-    })
-})
+        // create elements for weather day card
+        const cardDiv = $('<div>')
+        const cardBodyDiv = $('<div>')
+        const dateH2 = $('<h2>')
+        const iconImg = $('<img>')
+        const tempP = $('<p>')
+        const windP = $('<p>')
+        const humidityP = $('<p>')
+        // create text value for each element
+        dateH2.text(locRes.name + ' ' + dayjs(locRes.dt_txt).format('MM/DD/YYYY'))
+        iconImg.src = 'http://openweathermap.org/img/w/' + locRes.weather[0].icon + '.png';
+        console.log(iconImg.src);
+        iconImg.alt = 'weather icon';
+        tempP.text(locRes.main.temp + '°F')
+        windP.text(locRes.wind.speed + 'm/s')
+        humidityP.text(locRes.main.humidity + '%')
+        // add classes to each element
+        cardDiv.addClass('card w-100 g-0 m-0 gx-0')
+        cardBodyDiv.addClass('card-body g-0')
+        dateH2.addClass('card-title')
+        tempP.addClass('card-text')
+        windP.addClass('card-text')
+        humidityP.addClass('card-text')
+        // append elments to page
+        $('#weather').append(cardDiv)
+        cardDiv.append(cardBodyDiv)
+        cardBodyDiv.append(dateH2, iconImg, tempP, windP, humidityP)
+      })
+}
 
-var temps = [];
-function getWeather() {
-  fetch(weatherApiUrl)
+function getForecast() {
+  fetch(forecastApiUrl)
     .then(function (response) {
     if (response.ok) {
       return response.json();
@@ -58,7 +119,7 @@ function getWeather() {
         humidityP.text(locRes.list[i].main.humidity + '%')
         // add classes to each element
         cardDiv.addClass('card col g-0 m-0 gx-0')
-        cardBodyDiv.addClass('card-body')
+        cardBodyDiv.addClass('card-body g-0')
         dateH2.addClass('card-title')
         tempP.addClass('card-text')
         windP.addClass('card-text')
@@ -72,6 +133,9 @@ function getWeather() {
   })
 }
 
-
+function clear() {
+  $('#weather').text('')
+  $('forecast').text('')
+}
 // var dayData = data.list[i];
 // var date = dayjs(dayData.dt_txt).format('MM/DD/YYYY');
